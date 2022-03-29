f4cd81d4c448bee77bd9cbb93bd68f67df8196d7
marcopiii <marpi93@icloud.com>
2022-03-29 16:47:41 +0200

    feat: divide mobile and desktop theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -3,7 +3,13 @@ import * as typography from "./typography"
 import * as palette from "./palette"
 import * as components from "./components"
 
-export const warehouseMobileTheme = createTheme({
+export const mobileTheme = createTheme({
+  palette,
+  typography,
+  components,
+})
+
+export const desktopTheme = createTheme({
   palette,
   typography,
   components,
