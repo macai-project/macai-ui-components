ecd3e692e7aa33fa2df4b14a0ed93b7b43084dd4
marcopiii <marpi93@icloud.com>
2022-03-29 19:24:52 +0200

    feat: provide the theme to the stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,7 @@
+import React from "react"
+import { mobileTheme, ThemeProvider } from "../src"
+import { ThemeProvider as Emotion10ThemeProvider } from "emotion-theming"
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +11,14 @@ export const parameters = {
     },
   },
 }
+
+export const decorators = [
+  (Story, context) => (
+    // needed because of https://github.com/mui/material-ui/issues/24282#issuecomment-1065413808
+    <Emotion10ThemeProvider theme={mobileTheme}>
+      <ThemeProvider theme={mobileTheme}>
+        <Story {...context} />
+      </ThemeProvider>
+    </Emotion10ThemeProvider>
+  ),
+]
